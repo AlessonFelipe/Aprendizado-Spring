f7a451853241a0aac6b75f8ed5199b2b916eba5e
alesson <alesson.felipe@outlook.com>
2024-08-21 09:30:32 -0300

    Initial commit

---
+++ src/main/java/model/News.java
@@ -0,0 +1,12 @@
+package model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity(name = "tb_news")
+
+
+public class News extends BaseItem{
+
+}
---
+++ src/test/java/dio/Aprendizado_Spring/AprendizadoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.Aprendizado_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendizadoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
